6d0ff41c799a401f52fdae9f92891489bab0a614
唐正雄 <tzx1061225829@gmail.com>
2021-01-01 17:29:08 +0800

    添加时数据放在最上面

--- background.js
+++ background.js
@@ -2,15 +2,13 @@ let __DATA = [];
 
 init();
 
-chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse)
-{
-    
+chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
     const newData = [
-        ...__DATA,
         createNewData(JSON.parse(request.data)),
+        ...__DATA,
     ]
     __DATA = await setData('data', newData) || [];
-	sendResponse('我是后台，我已收到你的消息：' + JSON.stringify(request));
+    sendResponse('我是后台，我已收到你的消息：' + JSON.stringify(request));
 });
 
 
@@ -21,8 +19,8 @@ async function init() {
 function createNewData(data) {
     return {
         ...data,
-        id: + new Date(),
-        createTime: + new Date(),
+        id: +new Date(),
+        createTime: +new Date(),
     };
 }
 
@@ -33,8 +31,12 @@ function getAllData() {
 // 存储数据
 function setData(key, value) {
     return new Promise((resolve, reject) => {
-        chrome.storage.sync.set({[key]: value}, function(items) {
-            chrome.storage.sync.get({data: []}, function(items) {
+        chrome.storage.sync.set({
+            [key]: value
+        }, function (items) {
+            chrome.storage.sync.get({
+                data: []
+            }, function (items) {
                 resolve(items ? items[key] : null);
             });
         });
@@ -43,7 +45,9 @@ function setData(key, value) {
 // 获取数据
 function getData(key) {
     return new Promise((resolve, reject) => {
-        chrome.storage.sync.get({[key]: undefined}, function(items) {
+        chrome.storage.sync.get({
+            [key]: undefined
+        }, function (items) {
             resolve(items ? items[key] : null);
         });
     });
